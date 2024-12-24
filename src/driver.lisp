cb82acc1741b8db16319a8f0a6dcf612ce8c080e
KG <41345727+kg583@users.noreply.github.com>
2024-12-24 13:28:48 -0600

    Update driver.lisp

--- src/driver.lisp
+++ src/driver.lisp
@@ -1,13 +1,13 @@
-; Read program from STDIN
+; Read program from STDIN (single line only)
 (defvar *program* (read-line))
 
-; Initial input is nil
-(setq data nil)
+; Initial tape is nil
+(setq tape nil)
 
 ; Loop forever (until error)
 (loop
  (format t
-         "~{~v,'^~:*~a~}" ; Print only the portion before the first ETB
+         "~{~v,'^~:*~a~}" ; Print only the portion before the first ETB (there are Lispier ways to do this...)
          (setq data (coerce
-                     (format nil *program* data) ; Format the data
+                     (format nil *program* tape) ; Write the new tape
                      'list))))                   ; Coerce to a list since FORMAT can't loop over strings
