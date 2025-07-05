7f9ba9100395e7890b2f3d4a437e2df37abc9741
Ryan Pitasky <ryanpitasky+github@gmail.com>
2025-07-05 03:14:30 -0400

    7mat: parse struct definitions
    Also includes improved handling of negative numbers, assignments

--- 7MAT/src/parse.py
+++ 7MAT/src/parse.py
@@ -3,12 +3,19 @@ from pathlib import Path
 
 parser = Lark.open(Path(__file__).parent / "7mat.lark", propagate_positions=True, parser="lalr")
 
-test = """func my_func(Int_32 my_var) -> Dec {
+test = """
+struct my_struct {
+    Int_16[10] hexArray;
+}
+
+func my_func(Int_36 my_var, Int_16 value, struct my_struct hex) -> Int_36 {
+    hex.hexArray[5] = value;
     if (my_var == 10) { // conditional path taken
-        return 2 * my_var + 1;
+        return 2 * my_var + -1;
     } else {
         return 2;
     }
 }
 """
+
 print(parser.parse(test).pretty())
