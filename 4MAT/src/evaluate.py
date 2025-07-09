0a8bef26abdccf59429c84c6e5f47e084e4fe35c
KG <kgscience@hotmail.com>
2025-07-09 01:12:09 -0500

    4MAT: `~c` , `~p`, and most of `~r`

--- 4MAT/src/evaluate.py
+++ 4MAT/src/evaluate.py
@@ -1,5 +1,39 @@
+import unicodedata
+
 from .directives import *
 from .parse import parse, tokenize
+
+
+DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def char_name(char: str) -> str:
+    match char:
+        # Standard characters
+        case "\n":
+            return "Newline"
+
+        case " ":
+            return "Space"
+
+        # Semi-standard characters
+        case "\t":
+            return "Tab"
+
+        case "\f":
+            return "Page"
+
+        case "\x7f":
+            return "Rubout"
+
+        case "\a":
+            return "Linefeed"
+
+        case "\b":
+            return "Backspace"
+
+        case _:
+            return unicodedata.name(char).title()
 
 
 class Interpreter:
@@ -47,7 +81,7 @@ class Interpreter:
 
     def get_param(self, directive, index, default: int | str | None = 0):
         if 0 <= index < len(directive.prefix_params):
-            match directive[index]:
+            match directive.prefix_params[index]:
                 case Special.V:
                     arg = self.consume_arg()
                     if isinstance(arg, int) or isinstance(arg, str) and len(arg) == 1:
@@ -58,6 +92,8 @@ class Interpreter:
                     return len(self.args) - self.arg_idx
                 case None:
                     return default
+                case _:
+                    return directive.prefix_params[index]
         else:
             return default
 
@@ -81,6 +117,21 @@ class Interpreter:
                 self.print_repeated(directive, "~")
 
             # FORMAT Radix Control
+            case 'r':
+                self.eval_radix(directive)
+            case 'd':
+                directive.prefix_params.insert(0, 10)
+                self.eval_radix(directive)
+            case 'b':
+                directive.prefix_params.insert(0, 2)
+                self.eval_radix(directive)
+            case 'o':
+                directive.prefix_params.insert(0, 8)
+                self.eval_radix(directive)
+            case 'x':
+                directive.prefix_params.insert(0, 16)
+                self.eval_radix(directive)
+
             # FORMAT Floating-Point Printers
             # FORMAT Layout Control
             # FORMAT Control-Flow Operations
@@ -102,7 +153,16 @@ class Interpreter:
 
     # FORMAT Basic Output
     def eval_character(self, directive: Directive):
-        return  # unimplemented
+        char = self.consume_arg()
+
+        if not isinstance(char, str) or len(char) != 1:
+            raise TypeError("~c arg is not a character")
+
+        if directive.colon:
+            self.output(char_name(char))
+
+        else:
+            self.output(("#\\" if directive.at_sign else "") + char)
 
     def print_repeated(self, directive: Directive, char: str):
         self.output(char * self.get_param(directive, 0, default=1))
@@ -118,24 +178,69 @@ class Interpreter:
 
         self.output("\n" * (count - 1))
 
+    # FORMAT Radix Control
+    def eval_radix(self, directive: Directive):
+        base = self.get_param(directive, 0, default=None)
+        arg = self.consume_arg()
+
+        if not isinstance(arg, int):
+            # TODO: Print using ~a
+            return
+
+        if base is None:
+            # TODO: English names and such
+            return
+
+        elif not (isinstance(base, int) and 2 <= base <= 36):
+            raise ValueError("~r base must be an integer between 2 and 36")
+
+        # Actual numbers
+        min_col = self.get_param(directive, 1, default=0)
+        pad_char = self.get_param(directive, 2, default=" ")
+        comma_char = self.get_param(directive, 3, default=",")
+        comma_interval = self.get_param(directive, 4, default=3)
+
+        number = abs(arg)
+        output = []
+        length = 0
+
+        while number:
+            number, remainder = divmod(number, base)
+
+            if directive.colon and output and length % comma_interval == 0:
+                output = [DIGITS[remainder], comma_char, *output]
+
+            else:
+                output = [DIGITS[remainder], *output]
+
+            length += 1
+
+        if directive.at_sign and arg >= 0:
+            output = ["+", *output]
+
+        elif arg < 0:
+            output = ["-", *output]
+
+        self.output(f"{''.join(output):{pad_char}>{min_col}}")
+
     # FORMAT Control-Flow Operations
     def eval_goto(self, directive: Directive):
         if directive.at_sign:
             self.arg_idx = self.get_param(directive, 0, default=0)
             if self.arg_idx < 0:
-                print("Error: negative goto arg")
-                exit(1)
+                raise ValueError("negative ~* arg")
+
             self.clamp_arg_idx()
             return
 
         param = self.get_param(directive, 0, default=1)
         if param < 0:
-            print("Error: negative goto arg")
-            exit(1)
+            raise ValueError("negative ~* arg")
 
         if directive.colon:
             self.skip_args(-param)
             return
+
         self.skip_args(param)
 
     # FORMAT Miscellaneous Operations
@@ -144,9 +249,12 @@ class Interpreter:
             self.skip_args(-1)
 
         if directive.at_sign:
-            self.output(["ies", "y"][self.args[self.arg_idx] == 1])
-        elif self.args[self.arg_idx] == 1:
-            self.output("s")
+            options = ["ies", "y"]
+
+        else:
+            options = ["s", ""]
+
+        self.output(options[self.consume_arg() == 1])
 
 
 def fourmat(program: str | BlockDirective, args: list):
