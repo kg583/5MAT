633c9e244051b959e9caa3e60a197f0b4bc03fb0
Ryan Pitasky <ryanpitasky+github@gmail.com>
2025-07-13 10:33:36 -0400

    4MAT: Include performance trace option

--- 4MAT/__main__.py
+++ 4MAT/__main__.py
@@ -1,5 +1,23 @@
 from src import *
+import sys
 
 
+def main():
+    print(fivemat(open(sys.argv[-1], encoding="UTF-8").read()))
+
+"""
+arg options...
+ - perf <path to 5MAT>
+   for a performance trace
+ - <path to 5MAT>
+   just to run the file
+"""
 if __name__ == "__main__":
-    print(fourmat("~:[{~;[~]~:{~S~:[)~S~;~*~]~:^ ~}~:[~; ~]~{~S)~^ ~}~:[~; ~]~[~*~;)~S~;)~*~]~:[}~;]~]", [3]))
+    if len(sys.argv) > 1 and sys.argv[1] == "perf":
+        import cProfile
+        import pstats
+
+        cProfile.run("main()", filename="profile.pstats")
+        pstats.Stats("profile.pstats").print_stats()
+    else:
+        main()
