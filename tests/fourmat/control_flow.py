45aab08ceda10d8de7c82dc4d6ee5e58b3fa583d
KG <kgscience@hotmail.com>
2025-11-09 16:03:24 -0500

    4MAT: Add conditional tests from the spec

--- tests/fourmat/control_flow.py
+++ tests/fourmat/control_flow.py
@@ -3,6 +3,19 @@ import unittest
 from lib.fourmat.evaluate import *
 
 
+class ControlFlowSpecExamples(unittest.TestCase):
+    def test_conditional(self):
+        self.assertEqual(fourmat("~@[ print level = ~D~]~@[ print length = ~D~]", [None, 5]), " print length = 5")
+
+        # The HyperSpec erroneously includes a space before the comma
+        foo = "Items:~#[ none~; ~S~; ~S and ~S~:;~@{~#[~; and~] ~S~^,~}~]."
+        self.assertEqual(fourmat(foo, []), "Items: none.")
+        self.assertEqual(fourmat(foo, ["FOO"]), "Items: FOO.")
+        self.assertEqual(fourmat(foo, ["FOO", "BAR"]), "Items: FOO and BAR.")
+        self.assertEqual(fourmat(foo, ["FOO", "BAR", "BAZ"]), "Items: FOO, BAR, and BAZ.")
+        self.assertEqual(fourmat(foo, ["FOO", "BAR", "BAZ", "QUUX"]), "Items: FOO, BAR, BAZ, and QUUX.")
+
+
 class ControlFlow(unittest.TestCase):
     def test_goto(self):
         self.assertEqual(fourmat("~3*~c", ['a', 'b', 'c', 'd']), "d")
