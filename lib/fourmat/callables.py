11510e6ec672bfb6687bb09b03f8d98f0f520a73
KG <kgscience@hotmail.com>
2025-11-12 10:59:16 -0500

    4MAT: Add `read-line`

--- lib/fourmat/callables.py
+++ lib/fourmat/callables.py
@@ -27,7 +27,16 @@ def read_char_no_hang(*args, **kwargs) -> str:
     return read_char(*args, **kwargs)
 
 
-# read-byte doesn't take recursive-p so it doesn't apply here
+@lisp_function("READ-LINE")
+def read_line(stream=sys.stdin, eof_error_p=True, eof_value=None, _recursive_p=False) -> str:
+    line = stream.readline()
+
+    if line == "":
+        if eof_error_p:
+            raise EOFError("End of file reached while reading character")
+        return eof_value
+
+    return line.rstrip("\n")
 
 
 __all__ = {"lisp_functions"}
